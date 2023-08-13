176b1f31ba03d08ef399d048a98a31843b561b49
Destiny911gtr <dhanush911gtr@gmail.com>
2023-08-13 18:58:13 +0530

    Add MMKV flipper debugger plugin

--- App.js
+++ App.js
@@ -5,11 +5,13 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { initializeMMKVFlipper } from "react-native-mmkv-flipper-plugin";
+import { MMKV } from "react-native-mmkv";
 
 import StackNavigation from './src/components/navigator/stack-navigation';
 import LoadingScreen from './src/components/screens/loadingscreen';
@@ -18,8 +20,15 @@ import * as constants from './src/styles/colors/constants';
 import { realmConfig } from './src/utils/database';
 
 const { RealmProvider } = realmConfig;
+const storage = new MMKV();
 
 const App = () => {
+
+  useEffect(() => {
+    if (__DEV__) {
+      initializeMMKVFlipper({ default: storage });
+    }
+  }, [])
 
   return (
     <RealmProvider>
