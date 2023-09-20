85429dc4ed3a8736bab7fada19fee28111119e5c
Destiny911gtr <dhanush911gtr@gmail.com>
2023-09-20 23:12:25 +0530

    Enable codepush

--- App.js
+++ App.js
@@ -7,27 +7,33 @@
 
 import React, { useEffect } from 'react';
 
+import codePush from "react-native-code-push";
+import { MMKV } from "react-native-mmkv";
+import { initializeMMKVFlipper } from "react-native-mmkv-flipper-plugin";
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { initializeMMKVFlipper } from "react-native-mmkv-flipper-plugin";
-import { MMKV } from "react-native-mmkv";
 
-import StackNavigation from './src/navigation/stack-navigation';
 import LoadingScreen from './src/components/screens/loadingscreen';
-import { persistor, store } from './src/redux/store';
 import * as constants from './src/components/styles/colors/constants';
+import StackNavigation from './src/navigation/stack-navigation';
+import { persistor, store } from './src/redux/store';
 import { realmConfig } from './src/utils/database';
 
 const { RealmProvider } = realmConfig;
 const storage = new MMKV();
+const codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
 
-const App = () => {
+const MyApp = () => {
 
   useEffect(() => {
     if (__DEV__) {
       initializeMMKVFlipper({ default: storage });
     }
+    codePush.sync({
+      updateDialog: false,
+      installMode: codePush.InstallMode.ON_NEXT_RESTART
+    });
   }, [])
 
   return (
@@ -42,5 +48,7 @@ const App = () => {
     </RealmProvider>
   );
 }
+
+let App = codePush(codePushOptions)(MyApp);
 
 export default App;
